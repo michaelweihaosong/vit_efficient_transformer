7634d4b3dc1bec0357296b9a6d0a96b51c7b7423
Weihao Song <mike.song0724@gmail.com>
2023-12-02 14:43:34 +0000

    added new runs, checkpts, and analysis in notebook

--- batch_run.sh
+++ batch_run.sh
@@ -6,27 +6,29 @@
 # --dataset Caltech101 \
 # --batch-size 32 \
 # --test-batch-size 256 \
+# --save-model \
 # > ./logs/Caltech101/vit_transformer_patch_size_8.log
 
 # ViT performer
 
-python vit_efficient_transformer.py \
---model softmax \
---nb_features 256 \
---redraw 1000 \
---dataset Caltech101 \
---batch-size 32 \
---test-batch-size 256 \
-> ./logs/Caltech101/vit_performer_patch_size_8_softmax_nb_features_256_feature_redraw_interval_1000.log
+# python vit_efficient_transformer.py \
+# --model softmax \
+# --nb_features 256 \
+# --redraw 1000 \
+# --dataset Caltech101 \
+# --batch-size 32 \
+# --test-batch-size 256 \
+# > ./logs/Caltech101/vit_performer_patch_size_8_softmax_nb_features_256_feature_redraw_interval_1000.log
 
 python vit_efficient_transformer.py \
 --model relu \
---nb_features 256 \
+--nb_features 64 \
 --redraw 1000 \
 --dataset Caltech101 \
 --batch-size 32 \
 --test-batch-size 256 \
-> ./logs/Caltech101/vit_performer_patch_size_8_relu_nb_features_256_feature_redraw_interval_1000.log
+--save-model \
+> ./logs/Caltech101/vit_performer_patch_size_8_relu_nb_features_64_feature_redraw_interval_1000.log
 
 # python vit_efficient_transformer.py \
 # --model softmax \
@@ -38,14 +40,15 @@ python vit_efficient_transformer.py \
 # > ./logs/Caltech101/vit_performer_patch_size_8_softmax_nb_features_64_feature_redraw_interval_1000.log
 
 # # ViT linformer
-# python vit_efficient_transformer.py \
-# --model linformer \
-# --dataset Caltech101 \
-# --batch-size 32 \
-# --test-batch-size 256 \
-# --k_linformer 64 \
-# --one_kv_head \
-# > ./logs/Caltech101/vit_linformer_patch_size_8_k_64_one_kv_head_True.log
+python vit_efficient_transformer.py \
+--model linformer \
+--dataset Caltech101 \
+--batch-size 32 \
+--test-batch-size 256 \
+--k_linformer 64 \
+--one_kv_head \
+--save-model \
+> ./logs/Caltech101/vit_linformer_patch_size_8_k_64_one_kv_head_True.log
 
 # python vit_efficient_transformer.py \
 # --model linformer \
@@ -66,14 +69,15 @@ python vit_efficient_transformer.py \
 # > ./logs/Caltech101/vit_reformer_patch_size_8_n_bucket_size_4_n_hashes_2.log
 
 
-# python vit_efficient_transformer.py \
-# --model reformer \
-# --dataset Caltech101 \
-# --batch-size 32 \
-# --n_bucket_size 16 \
-# --n_hashes 2 \
-# --test-batch-size 256 \
-# > ./logs/Caltech101/vit_reformer_patch_size_8_n_bucket_size_16_n_hashes_2.log
+python vit_efficient_transformer.py \
+--model reformer \
+--dataset Caltech101 \
+--batch-size 32 \
+--n_bucket_size 16 \
+--n_hashes 2 \
+--test-batch-size 256 \
+--save-model \
+> ./logs/Caltech101/vit_reformer_patch_size_8_n_bucket_size_16_n_hashes_2.log
 
 
 # # ViT longformer
@@ -85,13 +89,14 @@ python vit_efficient_transformer.py \
 # --test-batch-size 256 \
 # > ./logs/Caltech101/vit_longformer_patch_size_8_attention_window_256.log
 
-# python vit_efficient_transformer.py \
-# --model longformer \
-# --dataset Caltech101 \
-# --batch-size 32 \
-# --attention_window 128 \
-# --test-batch-size 256 \
-# > ./logs/Caltech101/vit_longformer_patch_size_8_attention_window_128.log
+python vit_efficient_transformer.py \
+--model longformer \
+--dataset Caltech101 \
+--batch-size 32 \
+--attention_window 128 \
+--test-batch-size 256 \
+--save-model \
+> ./logs/Caltech101/vit_longformer_patch_size_8_attention_window_128.log
 
 
 # # ViT Nystromformer
@@ -104,19 +109,51 @@ python vit_efficient_transformer.py \
 # --pinv_iterations 6 \
 # > ./logs/Caltech101/vit_nystromformer_patch_size_8_num_landmarks_128_pinv_iterations_6.log
 
-# python vit_efficient_transformer.py \
-# --model nystromformer \
-# --dataset Caltech101 \
-# --batch-size 32 \
-# --test-batch-size 256 \
-# --num_landmarks 32 \
-# --pinv_iterations 6 \
-# > ./logs/Caltech101/vit_nystromformer_patch_size_8_num_landmarks_32_pinv_iterations_6.log
+python vit_efficient_transformer.py \
+--model nystromformer \
+--dataset Caltech101 \
+--batch-size 32 \
+--test-batch-size 256 \
+--num_landmarks 32 \
+--pinv_iterations 6 \
+--save-model \
+> ./logs/Caltech101/vit_nystromformer_patch_size_8_num_landmarks_32_pinv_iterations_6.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 # Caltech256
+
+# ViT transformer
+# python vit_efficient_transformer.py \
+# --model transformer \
+# --dataset Caltech256 \
+# --batch-size 32 \
+# --test-batch-size 256 \
+# --save-model \
+# > ./logs/Caltech256/vit_transformer_patch_size_8.log
 
 # # ViT linformer
 # python vit_efficient_transformer.py \
@@ -196,20 +233,20 @@ python vit_efficient_transformer.py \
 
 # Performer
 
-python vit_efficient_transformer.py \
---model softmax \
---nb_features 256 \
---redraw 1000 \
---dataset Caltech256 \
---batch-size 32 \
---test-batch-size 256 \
-> ./logs/Caltech256/vit_performer_patch_size_8_softmax_nb_features_256_feature_redraw_interval_1000.log
+# python vit_efficient_transformer.py \
+# --model softmax \
+# --nb_features 256 \
+# --redraw 1000 \
+# --dataset Caltech256 \
+# --batch-size 32 \
+# --test-batch-size 256 \
+# > ./logs/Caltech256/vit_performer_patch_size_8_softmax_nb_features_256_feature_redraw_interval_1000.log
 
-python vit_efficient_transformer.py \
---model relu \
---nb_features 256 \
---redraw 1000 \
---dataset Caltech256 \
---batch-size 32 \
---test-batch-size 256 \
-> ./logs/Caltech256/vit_performer_patch_size_8_relu_nb_features_256_feature_redraw_interval_1000.log
+# python vit_efficient_transformer.py \
+# --model relu \
+# --nb_features 256 \
+# --redraw 1000 \
+# --dataset Caltech256 \
+# --batch-size 32 \
+# --test-batch-size 256 \
+# > ./logs/Caltech256/vit_performer_patch_size_8_relu_nb_features_256_feature_redraw_interval_1000.log
